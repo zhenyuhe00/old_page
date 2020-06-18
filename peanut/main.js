2138a472869c7162a42f1b1f25db67dcc7f692c8
hzy-hh <837685486@qq.com>
2020-06-18 10:58:31 +0800

    Site updated: 2020-06-18 10:58:30

---
+++ peanut/main.js
@@ -0,0 +1,25 @@
+/**var timestamp = Date.parse(new Date());**/
+var time = (Date.parse("2020/02/14") - Date.parse("2020/01/11"))/1000 + 43200;
+
+function timedCount()
+{
+document.getElementById('shijian').innerHTML = fmoney(time,0);
+time = time + 1;
+t = setTimeout("timedCount()",1000);
+}
+
+/** ÿ��λ��������һ�����ţ������Ķ� **/
+
+function fmoney(s, n)   
+{   
+   n = n > 0 && n <= 20 ? n : 2;   
+   s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";   
+   var l = s.split(".")[0].split("").reverse(),   
+   r = s.split(".")[1];   
+   t = "";   
+   for(i = 0; i < l.length; i ++ )   
+   {   
+      t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");   
+   }   
+   return t.split("").reverse().join("") /*+ "." + r*/;   
+}
